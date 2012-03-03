36669eb1cb97801cd0b6bab20f088c432830c9fc
Mohamed Suhaib Fahad <mohamedsuhaiba@live.in>
2012-03-03 16:38:44 +0530

    Added formatting option in BuildTask

--- build/Pit.Build/CompilerTask.fs
+++ build/Pit.Build/CompilerTask.fs
@@ -11,7 +11,7 @@ open System.Xml
 open System.Xml.Linq
 open System.Linq
 
-module Helper = 
+module Helper =
 
     let pitEnvVarialbe = "PitLocation"
 
@@ -27,23 +27,23 @@ module Helper =
 
     let pitJsPath = Path.Combine(binPath, "Pit.js")
 
-    let getGuid (guidString: string) = 
+    let getGuid (guidString: string) =
         new Guid(guidString)
 
-    let xname1 name nameSpace =  
-        XName.Get(name , nameSpace)   
+    let xname1 name nameSpace =
+        XName.Get(name , nameSpace)
 
     let xname2 name = XName.Get(name)
 
-    let getRegexMatches text = 
-        Regex.Matches(text, slnRegex)    
+    let getRegexMatches text =
+        Regex.Matches(text, slnRegex)
 
-    let getFileString fileName = 
+    let getFileString fileName =
         let execAssembly = System.Reflection.Assembly.GetExecutingAssembly()
         using(execAssembly.GetManifestResourceStream(fileName))(fun ms ->
             let stream = new System.IO.StreamReader(ms)
             let text = stream.ReadToEnd()
-            stream.Close() 
+            stream.Close()
             text
         )
 
@@ -52,20 +52,20 @@ module Helper =
         using(execAssembly.GetManifestResourceStream(fileName))(fun ms ->
             XDocument.Load(ms))
 
-    let writeFile (path: String) (textToWrite: String) = 
+    let writeFile (path: String) (textToWrite: String) =
         let sw = new StreamWriter(path)
         sw.Write(textToWrite)
         sw.Close()
-        
+
     let getFsFilePathsInProject (xDoc:XDocument) =
         xDoc.Descendants(xname1 "Compile" xDoc.Root.Name.NamespaceName)
             .Attributes(xname2 "Include")
             |> Seq.filter (fun k -> k.Value.EndsWith(".fs"))
-            |> Seq.map (fun l -> 
+            |> Seq.map (fun l ->
                     let p = new DirectoryInfo(l.Value)
                     p.FullName
                     ) |> Seq.toArray
-    
+
     let copyAndOverWriteFile sourcePath destPath =
         File.Copy(sourcePath, destPath, true)
 
@@ -73,26 +73,26 @@ module Helper =
         if not(File.Exists(destPath)) then
             File.Copy(sourcePath, destPath, false)
 
-    let createDirectory (directoryPath:String) (directoryName:String) =  
+    let createDirectory (directoryPath:String) (directoryName:String) =
         let dirs = Directory.GetDirectories(directoryPath, directoryName, SearchOption.TopDirectoryOnly)
         if dirs.Count() = 0 then
             Directory.CreateDirectory(Path.Combine(directoryPath, directoryName)) |> ignore
 
-    let getSLGuids (xDoc:XDocument) = 
+    let getSLGuids (xDoc:XDocument) =
         let slDescendants = xDoc.Descendants(xname1 slAppListString xDoc.Root.Name.NamespaceName)
         if slDescendants.Count() > 0 then
-            slDescendants.First().Value.Split(',') 
+            slDescendants.First().Value.Split(',')
             |> Array.map (fun kk -> kk.Split('|').[0])
         else [||]
 
-    let getProjGuids (xDoc:XDocument) = 
+    let getProjGuids (xDoc:XDocument) =
         let slDescendants = xDoc.Descendants(xname1 "ProjectReference" xDoc.Root.Name.NamespaceName)
         slDescendants |> Seq.map(fun l -> getGuid(l.Descendants(xname1 "Project" xDoc.Root.Name.NamespaceName).First().Value)) |> Seq.toArray
 
     let getFilesNotPresentInProject (files:String[]) (xDoc:XDocument) (elementString:String) =
         let cFiles = xDoc.Descendants(xname1 elementString xDoc.Root.Name.NamespaceName)
-                        .Attributes(xname2 "Include")    
-                        |> Seq.map (fun l -> 
+                        .Attributes(xname2 "Include")
+                        |> Seq.map (fun l ->
                         let p = new DirectoryInfo(l.Value)
                         p.FullName) |> Seq.toArray
         let filesToAdd = files |> Array.filter (fun fl ->
@@ -109,8 +109,8 @@ module Helper =
     let addReferencesToProject (elementString:String) (xDoc:XDocument) (asms)=
         let itemGroupElement = new XElement(xname1 "ItemGroup" xDoc.Root.Name.NamespaceName)
         xDoc.Root.Add(itemGroupElement)
-        asms |> Array.iter (fun (k:String, l) -> 
-                                    let getEl = 
+        asms |> Array.iter (fun (k:String, l) ->
+                                    let getEl =
                                         let el = new XElement(xname1 (elementString) xDoc.Root.Name.NamespaceName, new XAttribute(xname2 "Include", (k.Replace(".dll", ""))))
                                         let r = new XElement(xname1 "HintPath" xDoc.Root.Name.NamespaceName)
                                         r.Value <- l
@@ -119,8 +119,8 @@ module Helper =
                                     itemGroupElement.Add(getEl)
                                     )
 
-        
-    let assemblies (projName:String) = 
+
+    let assemblies (projName:String) =
         let xdoc = XDocument.Load(projName)
         let rootEl = xdoc.Root
         let els = rootEl.Elements().Where((fun (s : XElement) -> s.Name.LocalName = "ItemGroup"))
@@ -131,26 +131,26 @@ module Helper =
                 c
             )
             |> Seq.filter(fun (x : XElement[])-> x.Length > 0)
-                 
+
         seq {
                 for x in asmEls do
-                    for xe in x do                        
+                    for xe in x do
                         let asmName = xe.Attribute(xname2 "Include").Value
                         yield  asmName + ".dll", Path.Combine(binPath, asmName + ".dll")
             }
             |> Seq.filter(fun (x, y) -> x <> String.Empty && x.EndsWith(".dbg.dll"))
             |> Seq.map (fun (x, y) -> x.Replace(".dbg", ""), y.Replace(".dbg", ""))
             |> Seq.toArray
-               
 
-   
+
+
 type CompilerTask() =
-    inherit Task()      
+    inherit Task()
 
-    let processInfo = 
+    let processInfo =
         new ProcessStartInfo(UseShellExecute = false, RedirectStandardOutput = true, RedirectStandardError = true, CreateNoWindow = true, WorkingDirectory = Directory.GetCurrentDirectory(), FileName = Helper.pfcPath)
 
-    let mutable config = String.Empty          
+    let mutable config = String.Empty
     let mutable solution = String.Empty
     let mutable slGuid = String.Empty
     let mutable xapName = String.Empty
@@ -158,8 +158,9 @@ type CompilerTask() =
     let mutable isTest = String.Empty
     let mutable outputPath = String.Empty
     let mutable projType = String.Empty
+    let mutable format = "true"
 
-    member x.Configuration 
+    member x.Configuration
         with get() = config
         and set(v) =
             config <- v
@@ -188,14 +189,19 @@ type CompilerTask() =
         with get() = pFilePath
         and set(v) =
             pFilePath <- v
-    
+
     member x.XapName
         with get() = xapName
         and set(v) =
             xapName <- v
 
+    member x.Format
+        with get() = format
+        and set(v) =
+            format <- v
+
     override this.Execute() =
-        try 
+        try
             let solutionFolder = Path.GetDirectoryName(this.SlnPath)
             let matches =  File.ReadAllText(this.SlnPath) |> Helper.getRegexMatches
             for i in 0..matches.Count-1 do
@@ -203,36 +209,36 @@ type CompilerTask() =
                 let projectPathOnDisk = Path.GetFullPath(Path.Combine(solutionFolder, projectPathRelativeToSolution))
                 let projectFile = projectPathRelativeToSolution
                 let projectGUID = matches.[i].Groups.["ProjectGUID"].Value |> Helper.getGuid
-                let projectDirectory = Path.GetDirectoryName(projectPathOnDisk)                           
-                let pd = Path.GetDirectoryName(this.ProjPath )                
+                let projectDirectory = Path.GetDirectoryName(projectPathOnDisk)
+                let pd = Path.GetDirectoryName(this.ProjPath )
                 if not(Directory.Exists(Path.Combine(pd, "obj\\PitPackage"))) then Directory.CreateDirectory(Path.Combine(pd, "obj\\PitPackage")) |> ignore
                 if projectGUID <> Helper.getGuid(this.ProjGuid) then
-                    let xDoc = XDocument.Load(projectPathOnDisk)                    
+                    let xDoc = XDocument.Load(projectPathOnDisk)
                     let scriptsPath = Path.Combine(projectDirectory, "Scripts")
                     let projName = this.XapName.Replace(".xap", "")
-                    Helper.getSLGuids xDoc |> Array.iter (fun guid -> 
-                        if not(String.IsNullOrEmpty(guid)) && Helper.getGuid(guid) = Helper.getGuid(this.ProjGuid) then                          
+                    Helper.getSLGuids xDoc |> Array.iter (fun guid ->
+                        if not(String.IsNullOrEmpty(guid)) && Helper.getGuid(guid) = Helper.getGuid(this.ProjGuid) then
                                 match this.Configuration with
-                                | "Debug" ->                                    
-                                    let jsDestPath =  Path.Combine(scriptsPath, projName + ".Silverlight.js")      
+                                | "Debug" ->
+                                    let jsDestPath =  Path.Combine(scriptsPath, projName + ".Silverlight.js")
                                     if not(File.Exists(jsDestPath)) then
-                                        Helper.getFileString("Pit.Silverlight.js.template").Replace("<%clientxap%>", "clientXaps=" + this.XapName) |> Helper.writeFile jsDestPath  
-                                    let slnDestPath =  Path.Combine(Path.Combine(projectDirectory, "ClientBin"), this.XapName)    
+                                        Helper.getFileString("Pit.Silverlight.js.template").Replace("<%clientxap%>", "clientXaps=" + this.XapName) |> Helper.writeFile jsDestPath
+                                    let slnDestPath =  Path.Combine(Path.Combine(projectDirectory, "ClientBin"), this.XapName)
                                     slnDestPath |> Helper.copyAndOverWriteFile (Path.Combine(this.OutputPath, this.XapName))
                                     let runTimeDestPath =  Path.Combine(Path.Combine(projectDirectory, "ClientBin"), "Pit.RunTime.xap")
                                     if not(File.Exists(runTimeDestPath)) then
-                                        runTimeDestPath |> Helper.copyAndAvoidOverWriteFile Helper.runtimeXapPath           
-                                | "Release" -> 
-                                    let pitDestPath =  Path.Combine(scriptsPath, "Pit.js")      
+                                        runTimeDestPath |> Helper.copyAndAvoidOverWriteFile Helper.runtimeXapPath
+                                | "Release" ->
+                                    let pitDestPath =  Path.Combine(scriptsPath, "Pit.js")
                                     if not(File.Exists(pitDestPath)) then
                                         Helper.copyAndOverWriteFile Helper.pitJsPath pitDestPath
                                     let asms = Helper.assemblies this.ProjPath
-                                    asms 
+                                    asms
                                     |> Seq.iter (fun (a, b) ->
                                         let pRes = Path.Combine(scriptsPath, a.Replace(".dll",".js"))
-                                        if not(File.Exists(pRes)) then 
+                                        if not(File.Exists(pRes)) then
                                             try
-                                                let args =  "\"" + b + "\"" + " /o:" + "\"" + pRes + "\"" + " /ft:true"                                  
+                                                let args =  "\"" + b + "\"" + " /o:" + "\"" + pRes + "\"" + " /ft:" + this.Format
                                                 processInfo.Arguments <- args
                                                 use pfbuild = Process.Start(processInfo)
                                                 let stdout = pfbuild.StandardOutput.ReadToEnd()
@@ -240,34 +246,34 @@ type CompilerTask() =
                                                 let err = pfbuild.StandardError.ReadToEnd()
                                                 if err.Length = 0 then
                                                     this.Log.LogMessage(MessageImportance.High, stdout)
-                                                else this.Log.LogError err                                  
+                                                else this.Log.LogError err
                                                 pfbuild.Close()
-                                            with e -> this.Log.LogErrorFromException(e)          
+                                            with e -> this.Log.LogErrorFromException(e)
                                         )
 
                                     let tProjPath = Path.Combine(Path.Combine(pd, "obj\\PitPackage"), "Pit.Library.fsproj")
-                                    let txDoc = Helper.getXDoc "Pit.Library.template" 
+                                    let txDoc = Helper.getXDoc "Pit.Library.template"
                                     let srcProj = XDocument.Load(this.ProjPath)
-                                    srcProj |> Helper.getFsFilePathsInProject |> Helper.addFilesToProject "Compile" txDoc  
+                                    srcProj |> Helper.getFsFilePathsInProject |> Helper.addFilesToProject "Compile" txDoc
                                     txDoc.Save(tProjPath)
-                                    asms |> Helper.addReferencesToProject "Reference" txDoc  
+                                    asms |> Helper.addReferencesToProject "Reference" txDoc
                                     txDoc.Save(tProjPath)
-                                    let jsOutPath = Path.Combine(Path.Combine(projectDirectory, "Scripts"), this.XapName.Replace(".xap", ".js"))   
-                                    try                                  
-                                        processInfo.Arguments <- "\"" + tProjPath + "\"" + " /o:" + "\"" + jsOutPath + "\"" + " /ft:true"
+                                    let jsOutPath = Path.Combine(Path.Combine(projectDirectory, "Scripts"), this.XapName.Replace(".xap", ".js"))
+                                    try
+                                        processInfo.Arguments <- "\"" + tProjPath + "\"" + " /o:" + "\"" + jsOutPath + "\"" + " /ft:" + this.Format
                                         let pfbuild = Process.Start(processInfo)
                                         let stdout = pfbuild.StandardOutput.ReadToEnd()
                                         pfbuild.WaitForExit()
                                         let err = pfbuild.StandardError.ReadToEnd()
-                                        if err.Length = 0 then                                        
+                                        if err.Length = 0 then
                                             this.Log.LogMessage(MessageImportance.High, stdout)
-                                        else 
-                                            this.Log.LogError err    
+                                        else
+                                            this.Log.LogError err
                                         pfbuild.Close()
-                                    with e -> this.Log.LogErrorFromException(e)                                            
-                                | _ -> () 
-                    )            
-            with ex -> this.Log.LogErrorFromException(ex)                
-        true 
-     
+                                    with e -> this.Log.LogErrorFromException(e)
+                                | _ -> ()
+                    )
+            with ex -> this.Log.LogErrorFromException(ex)
+        true
+
 
--- build/Pit.Build/LibCompilerTask.fs
+++ build/Pit.Build/LibCompilerTask.fs
@@ -11,16 +11,17 @@ open System.Xml
 open System.Xml.Linq
 open System.Linq
 
-   
-type LibCompilerTask() =
-    inherit Task()      
 
-    let processInfo = 
+type LibCompilerTask() =
+    inherit Task()
+
+    let create() =
         new ProcessStartInfo(UseShellExecute = false, RedirectStandardOutput = true, RedirectStandardError = true, CreateNoWindow = true, WorkingDirectory = Directory.GetCurrentDirectory(), FileName = Helper.pfcPath)
 
     let mutable outputPath = String.Empty
     let mutable solution = String.Empty
     let mutable slGuid = String.Empty
+    let mutable format = "true"
 
     member x.OutputPath
         with get() = outputPath
@@ -36,16 +37,22 @@ type LibCompilerTask() =
         with get() = slGuid
         and set(v) =
             slGuid <- v
-   
-    override this.Execute() =        
-        try          
-            let jsPath = this.OutputPath.Replace(".dll", ".js")                                 
-            processInfo.Arguments <- "\"" + this.OutputPath + "\"" + " /o:" + "\"" + (jsPath) + "\"" + " /ft:true"
+
+    member x.Format
+        with get() = format
+        and set(v) =
+            format <- v
+
+    override this.Execute() =
+        try
+            let jsPath = this.OutputPath.Replace(".dll", ".js")
+            let processInfo = create()
+            processInfo.Arguments <- "\"" + this.OutputPath + "\"" + " /o:" + "\"" + (jsPath) + "\"" + " /ft:" + this.Format
             let pfbuild = Process.Start(processInfo)
-            let stdout = pfbuild.StandardOutput.ReadToEnd()
             pfbuild.WaitForExit()
+            let stdout = pfbuild.StandardOutput.ReadToEnd()
             let err = pfbuild.StandardError.ReadToEnd()
-            if err.Length = 0 then     
+            if err.Length = 0 then
                 this.Log.LogMessage(MessageImportance.High, stdout)
                 let solutionFolder = Path.GetDirectoryName(this.SlnPath)
                 let matches =  File.ReadAllText(this.SlnPath) |> Helper.getRegexMatches
@@ -54,18 +61,18 @@ type LibCompilerTask() =
                     let projectPathOnDisk = Path.GetFullPath(Path.Combine(solutionFolder, projectPathRelativeToSolution))
                     let projectFile = projectPathRelativeToSolution
                     let projectGUID = matches.[i].Groups.["ProjectGUID"].Value |> Helper.getGuid
-                    let projectDirectory = Path.GetDirectoryName(projectPathOnDisk)                
-                    let pGuid = Helper.getGuid(this.ProjGuid)    
+                    let projectDirectory = Path.GetDirectoryName(projectPathOnDisk)
+                    let pGuid = Helper.getGuid(this.ProjGuid)
                     if projectGUID <> pGuid then
-                        let xDoc = XDocument.Load(projectPathOnDisk) 
-                        let k = xDoc |> Helper.getProjGuids                    
-                        if k.Contains(pGuid) then 
+                        let xDoc = XDocument.Load(projectPathOnDisk)
+                        let k = xDoc |> Helper.getProjGuids
+                        if k.Contains(pGuid) then
                             let scriptsPath = Path.Combine(projectDirectory, "Scripts")
                             if not(Directory.Exists(scriptsPath)) then Directory.CreateDirectory(scriptsPath) |> ignore
-                            File.Copy(jsPath , Path.Combine(scriptsPath, Path.GetFileName(jsPath)), true)                        
-            else 
-                this.Log.LogError err   
-            pfbuild.Close() 
-        with e -> this.Log.LogErrorFromException(e)          
+                            File.Copy(jsPath , Path.Combine(scriptsPath, Path.GetFileName(jsPath)), true)
+            else
+                this.Log.LogError err
+            pfbuild.Close()
+        with e -> this.Log.LogErrorFromException(e)
         true
 
